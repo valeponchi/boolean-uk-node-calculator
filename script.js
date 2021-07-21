d566b44ea8cebb7b42874a025b23667e9d978fd7
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-07-21 10:39:13 +0100

    error catching at the end - commented version

--- script.js
+++ script.js
@@ -27,3 +27,58 @@ function calculator(restOfArgs) {
 }
 
 console.log(calculator(restOfArgs))
+
+// const operators = [ "plus", "minus", "times", "divided"]
+
+// function parseInputs(params) {
+// const inputs = process.argv.slice(2)
+// console.log(inputs)
+// const firstNumber = Number.(inputs[0])
+// const secondNumber = Number.(inputs[2])
+// const operator = inputs[1]
+//
+// if (
+//    isNaN(firstNumber) ||
+//    isNaN(secondNumber) ||
+//    !operator.includes(operator)
+// ) {
+//   return null
+// }
+//  return [firstNumber, secondNumber, operator]
+// }
+
+//let firstNumber, secondNumber, operator = null
+
+// try {
+//   [firstNumber, secondNumber, operator] = parseInputs(process.argv)
+// } catch (error) {
+//   console.error("Wrong Inputs")
+//   process.exit(1)
+//}
+
+// function plus(number1, number2) {
+//  return number1 + number2
+//}
+
+// function minus(number1, number2) {
+//  return number1 - number2
+//}
+
+// function times(number1, number2) {
+//  return number1 * number2
+//}
+
+// function divided(number1, number2) {
+//  return number1 / number2
+//}
+
+//CREATE AN OBJ WITH THE KEYS BEING THE FUNCTIONS:
+// const operations = {
+//    times: times,
+//    divided: divided,
+//    plus: plus,
+//    minus: minus
+//}
+
+//const result = operations[operator](firstNumber, secondNumber)
+// console.log(result)
